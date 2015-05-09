3f04109b86d2a9f93742fabb181441eb821c32ee
Franco Vare <vare.franco@gmail.com>
2015-05-09 20:53:10 -0300

    creo que ya esta todo bien esto, segun lei lo que escribio el crack en el foro

--- src/pruebaScript.c
+++ src/pruebaScript.c
@@ -1,48 +1,48 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdio_ext.h>
 #include <stdlib.h>
 #include <unistd.h>
-// \
+#include <string.h>
 
-//void switchStdout(const char *newStream);
-//void revertStdout();
-
-//static int fd;
-//static fpos_t pos;
+void ejecutarScript(char* path, char* in, char* out);
 
 int main(void) {
-	//int sout = fileno(stdout);
-	//freopen("/home/utnso/Escritorio/file1", "a", stdout);
-	int bak, new;
-	fflush(stdout);
-	bak = dup(1);
-	new = open("/home/utnso/Escritorio/file1", O_WRONLY | O_APPEND | O_CREAT,S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-	//los O_ setean como se abre el archivo, los S_ dan permisos de lectura y escritura al usuario
-	dup2(new, 1);
-	close(new);
-	printf("asdasdasdasd\n");
-	fflush(stdout);
-	dup2(bak, 1);
-	close(bak);
-	printf("asd");
-	//dup2(sout, fileno(stdout));
-	//close(sout);
-	return 0;
+	int n;
+	const char* out = "fileOut.txt"; //GUARDA LO QUE EL SCRIPT TIRA POR STDOUT ACA
+	const char* in = "fileIn.txt";   // EL SCRIPT TOMA STDIN DE ACA
+	const char* path = "Nuevo.sh";   // ESTE ES EL SCRIPT
+	ejecutarScript(path,in,out);
+	printf("Ingresa un numero para probar stdin:\n");
+	scanf("%d",&n);
+	printf("me escribiste %d\n",n);
 }
 
-//void switchStdout(const char *newStream)
-//{
-//  fflush(stdout);
-//  fgetpos(stdout, &pos);
-//  fd = dup(fileno(stdout));
-//  freopen(newStream, "w", stdout);
-//}
 
-//void revertStdout()
-//{
-//  fflush(stdout);
-//  dup2(fd, fileno(stdout));
-//  close(fd);
-//  clearerr(stdout);
-//  fsetpos(stdout, &pos);
-//}
+//redirecciona STDOUT a out y STDIN a in y ejecuta el script,
+// cuando termina vuelve STDOUT y STDIN a la normalidad
+void ejecutarScript(char* path, char* in, char* out) {
+	char* command = (char*) malloc(strlen(path) + 3);
+	strcpy(command, "sh ");
+	strcat(command, path);
+	int bako, newo, baki, newi;
+	fflush(stdout);
+	//fflush(stdin);
+	__fpurge(stdin);
+	bako = dup(fileno(stdout)); //
+	baki = dup(fileno(stdin));  //
+	newo = open(out, O_WRONLY | O_APPEND);
+	newi = open(in, O_RDONLY); //  		esto me cambia los stdin y stdout originales por mis arhcivos
+	dup2(newo, fileno(stdout)); //
+	dup2(newi, fileno(stdin));  //
+	close(newo);  				//
+	close(newi);  				//
+	system(command);            // -> aca va mi codigo
+	fflush(stdout);             //
+	//fflush(stdin);
+	__fpurge(stdin);//
+	dup2(bako, fileno(stdout)); //
+	dup2(baki, fileno(stdin)); //  		esto me cambia los stdin y stdout de nuevo a los originales
+	close(bako);                //
+	close(baki);
+}
